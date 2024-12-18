aaf59dec38da58541c8ee11cbcd3f3c668083f9a
msk2906 <msk290697@gmail.com>
2024-12-18 19:33:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿public class program
+{
+    public static async Task Main(string[] args)
+    {
+        //PrinterTwo();
+        PrinterTwoAsync(); //  3 Sec Wait time
+        PrinterOne();
+        PrinterThree();
+        Console.WriteLine("\nMain program");
+        Console.ReadKey();
+    }
+    public static void PrinterOne()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("printer one");
+            Task.Delay(1000).Wait();
+        }
+    }
+
+    public static void PrinterTwo()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("printer two");
+            Task.Delay(1000).Wait();
+        }
+    }
+
+    public static void PrinterThree()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("printer Three");
+            Task.Delay(1000).Wait();
+        }
+    }
+
+    public static async Task PrinterTwoAsync()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Async Printer Two");
+                Task.Delay(3000).Wait();
+            }
+        });
+    }
+}
